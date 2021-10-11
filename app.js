f4863beee17bfe7698e84565321b7d83c9a3c4b0
Muhammad Al-amin <muhammad.alamindev01@gmail.com>
2021-10-12 02:31:42 +0600

    .env req

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oxeqo.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
